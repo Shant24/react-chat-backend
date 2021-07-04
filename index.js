894f153e204a4aa92f26006e52db8a5a50cc1012
Shant24 <shant.asf@gmail.com>
2021-07-04 15:02:15 +0400

    allow all origins

--- index.js
+++ index.js
@@ -12,12 +12,7 @@ const environment = process.env.NODE_ENV || 'development';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  }),
-);
+app.use(cors({ origin: '*', credentials: true }));
 app.use(logger('dev'));
 
 app.get('/', (req, res) => {
