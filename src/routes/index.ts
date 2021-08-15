d549c76108e9b5cd80444b0e63fd895e0b6a0621
Shant24 <shant.asf@gmail.com>
2021-08-15 18:40:43 +0400

    add routes

---
+++ src/routes/index.ts
@@ -0,0 +1,51 @@
+import { Express, Request, Response } from 'express';
+import axios, { AxiosResponse } from 'axios';
+
+const userRoutes = require('./user.routes');
+
+const PORT = process.env.PORT || 8080;
+const HOST_URL = process.env.CURRENT_HOST_URL;
+const environment = process.env.NODE_ENV || 'development';
+
+const fetchDB = async () => {
+  const url: string = `${environment === 'development' ? `${HOST_URL}:${PORT}` : HOST_URL}/db.json`;
+  const { data }: AxiosResponse = await axios.get(url);
+  return data;
+};
+
+const routes = {
+  init: (app: Express) => {
+    app.use(`/user`, userRoutes);
+
+    app.get('/', async (req: Request, res: Response) => {
+      const db = await fetchDB();
+      res.json(db);
+    });
+
+    app.get('/dialogues', async (req: Request, res: Response) => {
+      const db = await fetchDB();
+      res.json(db.dialogues);
+    });
+
+    app.get('/dialogues/:id', async (req: Request, res: Response) => {
+      const { id } = req.params;
+      const db = await fetchDB();
+      const dialoguesById = db.dialogues.filter((dialogue: any) => dialogue._id === id);
+      res.json(dialoguesById);
+    });
+
+    app.get('/messages', async (req: Request, res: Response) => {
+      const db = await fetchDB();
+      res.json(db.messages);
+    });
+
+    app.get('/messages/:id', async (req: Request, res: Response) => {
+      const { id } = req.params;
+      const db = await fetchDB();
+      const messagesById = db.messages.filter((message: any) => message.roomId === id);
+      res.json(messagesById);
+    });
+  },
+};
+
+export default routes;
